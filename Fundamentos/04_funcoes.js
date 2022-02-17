773956973b67e0348d85f82ef9d74bef1ca14ef1
Carlos Eduardo M Gomes <93795785+cemg-dev@users.noreply.github.com>
2022-02-17 14:06:28 -0300

    Update 04_funcoes.js

--- Fundamentos/04_funcoes.js
+++ Fundamentos/04_funcoes.js
@@ -23,3 +23,22 @@ imprime(sum);
     
 imprime(subtracao(n1, n2)); // o resultado de uma função pode ser imprimido diretamente
 
+/*
+* Em JavaScript existem ainda as funcoes construtoras e o prototype
+*
+
+// funcoes construtoras sempre tem a primeira letra maiuscula
+function Point(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+let point = new Point(1, 1);
+
+// podemos definir novos metodos para objetos Point
+point.prototype.novo_metodo = function() {
+    return Math.sqrt(
+        this.x * this.x +
+        this.y * this.y
+    );
+}
